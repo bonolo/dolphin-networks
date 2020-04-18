2d1b26531f20764d5b0e7fe252f0b6f5a9fd25ff
Kevin Cullen <bonolo@users.noreply.github.com>
2020-04-18 16:12:53 -0600

    Initial commit. Stub R file and .gitignore

---
+++ Cullen_Kevin_HW5.R
@@ -0,0 +1,42 @@
+#########
+# CIS576 HW5
+# Kevin Cullen
+# Network Visualization
+setwd("~/Projects/cis576/HW5")
+
+# library()  # all statements needed to load libs
+# library(tidyverse)
+# library(RColorBrewer)
+# library(reshape)
+
+# --------- Theme, scales, etc
+
+theme_set(theme_light())
+# light.grey <- "#dddddd"
+# light.grey.line <- element_line(size = 0.3, color = light.grey)
+# theme.base <- theme(panel.border = element_blank()
+#                     , panel.grid.major = element_blank()
+#                     , panel.grid.minor = element_blank()
+#                     , axis.ticks.x = element_blank()
+#                     , axis.ticks.y = element_blank()
+# )
+# palette25 <- colorRampPalette(brewer.pal(8, "Dark2"))(25)
+# palette2 <- c("#66C2A5", "#FC8D62")
+# palette2rev <- c("#FC8D62", "#66C2A5")
+
+options(scipen = 999)
+
+# --------- Load Text file, clean up, set options. ----------
+
+
+# -- Question --------------------------
+
+
+# -- Question -------------------------
+
+
+# -- Generate Tableau File(s) --------------------------
+# write.csv(frequencies.df[1:100,], file = "frequencies.csv", row.names = FALSE)
+
+
+##########
