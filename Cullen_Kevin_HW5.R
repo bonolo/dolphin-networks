78a87d4050ba177848e9539743896c76261691eb
Kevin Cullen <bonolo@users.noreply.github.com>
2020-04-22 22:24:51 -0600

    A couple of hairballs.

--- Cullen_Kevin_HW5.R
+++ Cullen_Kevin_HW5.R
@@ -8,6 +8,7 @@ setwd("~/Projects/cis576/HW5")
 # library(tidyverse)
 # library(RColorBrewer)
 # library(reshape)
+library(Matrix)
 
 # --------- Theme, scales, etc
 
@@ -27,6 +28,49 @@ theme_set(theme_light())
 options(scipen = 999)
 
 # --------- Load Text file, clean up, set options. ----------
+
+# http://networkrepository.com/soc-dolphins.php
+dolphins.m <- readMM("data/soc-dolphins.mtx")
+dolphins.df <- summary(dolphins.m)
+
+# http://networkrepository.com/eco-everglades.php
+everglades.df <- read.csv("data/eco-everglades.edges", header = FALSE, sep = " ")
+colnames(everglades.df) <- c("start", "end", "value")
+
+# build igraph object from CSV files
+dolphin_network <- graph_from_data_frame(dolphins.df, directed = FALSE
+                                         , vertices = union(dolphins.df$i, dolphins.df$j))
+
+everglades.net <- graph_from_data_frame(everglades.df
+                                        , vertices = union(everglades.df$start, everglades.df$end))
+
+dolphins.df %>% group_by(i, j) %>% tally()
+  # Find totals by CMTE_ID
+  # san_check.df <- donate.df %>%
+  #   group_by(CMTE_ID, CAND_ID, CAND_NAME) %>%
+  #   summarise(CASH_FOR_VOTES = sum(TRANSACTION_AMT))
+  # san_check.df$CASH_FOR_VOTES = scales::dollar(san_check.df$CASH_FOR_VOTES)
+
+# visualize graph
+set.seed(2020)
+ggraph(dolphin_network, layout = "fr") +
+  # geom_edge_link(alpha = 0.2, aes(width = value)) +
+  geom_edge_link(alpha = 0.2) +
+  # geom_node_point(aes(color = as.factor(group), size = 10 * nodesize)) +
+  geom_node_point(aes(size = 10)) +
+  geom_node_text(aes(label = name), repel = TRUE) +
+  theme_graph() +
+  theme(legend.position="none")
+
+
+ggraph(everglades.net, layout = "fr") +
+  geom_edge_link(alpha = 0.2, aes(width = value)) +
+  # geom_edge_link(alpha = 0.2) +
+  # geom_node_point(aes(color = as.factor(group), size = 10 * nodesize)) +
+  geom_node_point(aes(size = 10)) +
+  geom_node_text(aes(label = name), repel = TRUE) +
+  theme_graph() +
+  theme(legend.position="none")
 
 
 # -- Question --------------------------
