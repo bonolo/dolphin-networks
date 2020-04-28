cd77f2d6754096b6cc76f995ca72076d514bf899
Kevin Cullen <bonolo@users.noreply.github.com>
2020-04-27 22:23:31 -0600

    Hive-related settings moved after community detection.

--- Cullen_Kevin_HW5.R
+++ Cullen_Kevin_HW5.R
@@ -1,5 +1,5 @@
-#########
-# CIS576 HW5
+# ---- CIS576 HW5 ----
+#
 # Kevin Cullen
 # Network Visualization
 
@@ -20,7 +20,7 @@ library(grid)
 
 
 
-# ---- Sources ---- 
+# ---- References ---- 
 # Tutorial...
 # Static and dynamic network visualization with R, Katherine Ognyanova
 # https://kateto.net/network-visualization
@@ -96,39 +96,6 @@ igraph.net <- simplify(igraph.net, remove.multiple = F, remove.loops = T)
 # Plot with curved edges (edge.curved=.1)
 # plot(igraph.net, edge.curved = .1)
 
-# Calculate degree for all nodes (#links)
-degAll <- igraph::degree(igraph.net, v = V(igraph.net), mode = "all")
-# Calculate betweenness for all nodes
-betAll <- igraph::betweenness(igraph.net, v = V(igraph.net), directed = FALSE) / (((vcount(igraph.net) - 1) * (vcount(igraph.net)-2)) / 2)
-betAll.norm <- (betAll - min(betAll))/(max(betAll) - min(betAll))
-# Set node degree and betweenness
-igraph.net <- igraph::set.vertex.attribute(igraph.net, "degree", index = V(igraph.net), value = degAll)
-igraph.net <- igraph::set.vertex.attribute(igraph.net, "betweenness", index = V(igraph.net), value = betAll.norm)
-
-# Zero out edge weight and similarity
-igraph.net <- igraph::set.edge.attribute(igraph.net, "weight", index = E(igraph.net), value = 0)
-igraph.net <- igraph::set.edge.attribute(igraph.net, "similarity", index = E(igraph.net), value = 0)
-
-# Calculate Dice similarities between all pairs of nodes
-dsAll <- igraph::similarity.dice(igraph.net, vids = V(igraph.net), mode = "all")
-
-# Calculate edge weight based on the node similarity
-F1 <- function(x) {data.frame(V4 = dsAll[which(V(igraph.net)$name == as.character(x$from)), which(V(igraph.net)$name == as.character(x$to))])}
-dolphins.ext <- ddply(dolphins.df, .variables=c("from", "to"), function(x) data.frame(F1(x)))
-
-for (i in 1:nrow(dolphins.ext))
-{
-  # dolphin dataset has not weight
-  # E(igraph.net)[as.character(dolphins.ext$from) %--% as.character(dolphins.ext$to)]$weight <- as.numeric(dolphins.ext$V3)
-  E(igraph.net)[as.character(dolphins.ext$from) %--% as.character(dolphins.ext$to)]$similarity <- as.numeric(dolphins.ext$V4)
-}
-
-rm(degAll, betAll, betAll.norm, F1, dsAll, i)
-
-write.table(dolphins.ext, "dolphin_similarity.csv", row.names = FALSE, 
-            col.names = c("from", "to", "similarity"), sep=",")
-
-
 
 # ---- Network plots... loop layouts ----
 
@@ -170,7 +137,7 @@ class(clp)
 # which igraph knows how to plot:
 plot(clp,
      igraph.net,
-     layout = layout_with_fr, # layout_with_graphopt
+     layout = layout_with_fr, # layout_with_mds | layout_with_graphopt
      vertex.size = 5,
      vertex.label = NA)
 
@@ -222,6 +189,45 @@ plot(
 
 # ---- Hive Pre-work ----
 
+# ----~~ Set network attributes ----
+# When I put this code before community detection, the community results were
+# nuts. Coloring didn't make sense, tiny groups, etc.
+
+
+# Calculate degree for all nodes (#links)
+degAll <- igraph::degree(igraph.net, v = V(igraph.net), mode = "all")
+# Calculate betweenness for all nodes
+betAll <- igraph::betweenness(igraph.net, v = V(igraph.net), directed = FALSE) / (((vcount(igraph.net) - 1) * (vcount(igraph.net)-2)) / 2)
+betAll.norm <- (betAll - min(betAll))/(max(betAll) - min(betAll))
+# Set node degree and betweenness
+igraph.net <- igraph::set.vertex.attribute(igraph.net, "degree", index = V(igraph.net), value = degAll)
+igraph.net <- igraph::set.vertex.attribute(igraph.net, "betweenness", index = V(igraph.net), value = betAll.norm)
+
+# Zero out edge weight and similarity
+igraph.net <- igraph::set.edge.attribute(igraph.net, "weight", index = E(igraph.net), value = 0)
+igraph.net <- igraph::set.edge.attribute(igraph.net, "similarity", index = E(igraph.net), value = 0)
+
+# Calculate Dice similarities between all pairs of nodes
+dsAll <- igraph::similarity.dice(igraph.net, vids = V(igraph.net), mode = "all")
+
+# Calculate edge weight based on the node similarity
+F1 <- function(x) {data.frame(V4 = dsAll[which(V(igraph.net)$name == as.character(x$from)), which(V(igraph.net)$name == as.character(x$to))])}
+dolphins.ext <- ddply(dolphins.df, .variables=c("from", "to"), function(x) data.frame(F1(x)))
+
+for (i in 1:nrow(dolphins.ext))
+{
+  # dolphin dataset has no weight
+  # E(igraph.net)[as.character(dolphins.ext$from) %--% as.character(dolphins.ext$to)]$weight <- as.numeric(dolphins.ext$V3)
+  E(igraph.net)[as.character(dolphins.ext$from) %--% as.character(dolphins.ext$to)]$similarity <- as.numeric(dolphins.ext$V4)
+}
+
+rm(degAll, betAll, betAll.norm, F1, dsAll, i)
+
+write.table(dolphins.ext, "dolphin_similarity.csv", row.names = FALSE, 
+            col.names = c("from", "to", "similarity"), sep=",")
+
+
+
 # ----~~ node/edge color based on the properties ----
 
 # Calculate node size
@@ -269,8 +275,6 @@ hive4 <- mod.mineHPD(hive3, option = "remove zero edge")
 # And finally, plotting our graph (Figure 1)
 plotHive(hive4, method = "abs", bkgnd = "white", axLabs = c("source", "hub", "sink"), 
          axLab.gpar = gpar(col = c("red", "blue", "green")), axLab.pos = 10)
-
-
 
 
 
