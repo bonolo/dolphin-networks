6a2d7daf99123ce0b1d71671931a74d8e93a3400
Kevin Cullen <bonolo@users.noreply.github.com>
2020-04-27 22:08:46 -0600

    First working Hive.

--- Cullen_Kevin_HW5.R
+++ Cullen_Kevin_HW5.R
@@ -13,6 +13,8 @@ library(Matrix)
 library(igraph)
 library(RColorBrewer)
 library(plyr)
+library(HiveR)
+library(grid)
 # library(scales)
 # library(ggraph)
 
@@ -65,10 +67,28 @@ igraph.net <- graph_from_data_frame(dolphins.df, directed = FALSE
 
 # igraph.net <- graph_from_data_frame(thornbill.df, directed = FALSE
 #                                     , vertices = union(thornbill.df$start, thornbill.df$end))
+#                                     
 # class(igraph.net)
 # E(igraph.net)         # The edges of the "net" object
 # V(igraph.net)         # The vertices of the "net" object
 # E(igraph.net)$weight  # Edge attribute "weight"
+# 
+# --~~ Examine weight -----
+# Dolphin data set has no weight
+# hist(E(igraph.net)$weight, breaks = 25)
+# hist(E(igraph.net)$weight)
+# mean(E(igraph.net)$weight)
+# sd(E(igraph.net)$weight)
+
+# Keep edges with weight > mean
+# igraph.net.sp <- delete_edges(igraph.net, E(igraph.net)[weight < mean(E(igraph.net)$weight)])
+
+# Set edge width based on weight
+# E(igraph.net)$width <- E(igraph.net)$weight/10
+# E(igraph.net)$width <- E(igraph.net)$weight
+# E(igraph.net)$width <- rescale(E(igraph.net)$weight)
+
+
 
 # Remove loops in the graph
 igraph.net <- simplify(igraph.net, remove.multiple = F, remove.loops = T)
@@ -78,23 +98,19 @@ igraph.net <- simplify(igraph.net, remove.multiple = F, remove.loops = T)
 
 # Calculate degree for all nodes (#links)
 degAll <- igraph::degree(igraph.net, v = V(igraph.net), mode = "all")
-# Use degree to set node size:
-# V(igraph.net)$size <- degAll / 3
-
 # Calculate betweenness for all nodes
 betAll <- igraph::betweenness(igraph.net, v = V(igraph.net), directed = FALSE) / (((vcount(igraph.net) - 1) * (vcount(igraph.net)-2)) / 2)
 betAll.norm <- (betAll - min(betAll))/(max(betAll) - min(betAll))
-
+# Set node degree and betweenness
 igraph.net <- igraph::set.vertex.attribute(igraph.net, "degree", index = V(igraph.net), value = degAll)
 igraph.net <- igraph::set.vertex.attribute(igraph.net, "betweenness", index = V(igraph.net), value = betAll.norm)
 
-
+# Zero out edge weight and similarity
 igraph.net <- igraph::set.edge.attribute(igraph.net, "weight", index = E(igraph.net), value = 0)
 igraph.net <- igraph::set.edge.attribute(igraph.net, "similarity", index = E(igraph.net), value = 0)
 
 # Calculate Dice similarities between all pairs of nodes
 dsAll <- igraph::similarity.dice(igraph.net, vids = V(igraph.net), mode = "all")
-
 
 # Calculate edge weight based on the node similarity
 F1 <- function(x) {data.frame(V4 = dsAll[which(V(igraph.net)$name == as.character(x$from)), which(V(igraph.net)$name == as.character(x$to))])}
@@ -114,12 +130,8 @@ write.table(dolphins.ext, "dolphin_similarity.csv", row.names = FALSE,
 
 
 
+# ---- Network plots... loop layouts ----
 
-
-# Set edge width based on weight
-# E(igraph.net)$width <- E(igraph.net)$weight/10
-# E(igraph.net)$width <- E(igraph.net)$weight
-# E(igraph.net)$width <- rescale(E(igraph.net)$weight)
 
 # Set the network layout:
 graph_attr(igraph.net, "layout") <- layout_with_graphopt
@@ -144,17 +156,6 @@ for (layout in layouts) {
     main = layout
   )
 }
-
-
-# --~~ Examine weight -----
-# Dolphin data set has no weight
-# hist(E(igraph.net)$weight, breaks = 25)
-# hist(E(igraph.net)$weight)
-# mean(E(igraph.net)$weight)
-# sd(E(igraph.net)$weight)
-
-# Keep edges with weight > mean
-# igraph.net.sp <- delete_edges(igraph.net, E(igraph.net)[weight < mean(E(igraph.net)$weight)])
 
 
 # -- Community Detection -------
@@ -217,6 +218,58 @@ plot(
 
 # as.vector(V(igraph.net)[community == 1])
 # unique(V(igraph.net)$community)
+
+
+# ---- Hive Pre-work ----
+
+# ----~~ node/edge color based on the properties ----
+
+# Calculate node size
+# We'll interpolate node size based on the node betweenness centrality, using the "approx" function
+# And we will assign a node size for each node based on its betweenness centrality
+approxVals <- approx(c(0.5, 1.5), n = length(unique(V(igraph.net)$betweenness)))
+nodes_size <- sapply(V(igraph.net)$betweenness, function(x) approxVals$y[which(sort(unique(V(igraph.net)$betweenness)) == x)])
+rm(approxVals)
+
+# Define node color
+# We'll interpolate node colors based on the node degree using the "colorRampPalette" function from the "grDevices" library
+library("grDevices")
+# This function returns a function corresponding to a collor palete of "bias" number of elements
+F2 <- colorRampPalette(c("#F5DEB3", "#FF0000"), bias = length(unique(V(igraph.net)$degree)), space = "rgb", interpolate = "linear")
+# Now we'll create a color for each degree
+colCodes <- F2(length(unique(V(igraph.net)$degree)))
+# And we will assign a color for each node based on its degree
+nodes_col <- sapply(V(igraph.net)$degree, function(x) colCodes[which(sort(unique(V(igraph.net)$degree)) == x)])
+rm(F2, colCodes)
+
+# Assign visual attributes to edges using the same approach as we did for nodes
+F2 <- colorRampPalette(c("#FFFF00", "#006400"), bias = length(unique(E(igraph.net)$similarity)), space = "rgb", interpolate = "linear")
+colCodes <- F2(length(unique(E(igraph.net)$similarity)))
+edges_col <- sapply(E(igraph.net)$similarity, function(x) colCodes[which(sort(unique(E(igraph.net)$similarity)) == x)])
+rm(F2, colCodes)
+
+
+
+# ---- HiveR start ----
+
+# Create a hive plot from the data frame
+hive1 <- mod.edge2HPD(edge_df = dolphins.ext)
+#sumHPD(hive1)
+
+# Assign nodes to a radius based on their degree (number of edges they are touching)
+hive2 <- mod.mineHPD(hive1, option = "rad <- tot.edge.count")
+
+# Assign nodes to axes based on their position in the edge list 
+# (this function assumes direct graphs, so it considers the first column to be a source and second column to be a sink )
+hive3 <- mod.mineHPD(hive2, option = "axis <- source.man.sink")
+
+# Removing zero edges for better visualization 
+hive4 <- mod.mineHPD(hive3, option = "remove zero edge")
+
+# And finally, plotting our graph (Figure 1)
+plotHive(hive4, method = "abs", bkgnd = "white", axLabs = c("source", "hub", "sink"), 
+         axLab.gpar = gpar(col = c("red", "blue", "green")), axLab.pos = 10)
+
 
 
 
